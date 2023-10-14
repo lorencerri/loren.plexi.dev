0838a1d0834e6a45a47bd0464d289b18d725acca
Loren Cerri <lorencerri@gmail.com>
2023-10-14 12:20:56 -0400

    Update List.tsx

--- app/components/List.tsx
+++ app/components/List.tsx
@@ -87,7 +87,7 @@ export default function List() {
               Just a fun side project I spent a weekend on, but it allows you to
               play DOOM in a markdown file specifically on GitHub. It uses a web
               server to map endpoints to keyboard inputs, then renders the last
-              few frames of the game in a .gif image. You can [play it
+              few frames of the game in an embedded .gif image. You can [play it
               here](https://github.com/lorencerri). According to the
               [stats](https://github.com/lorencerri/play-doom#play-doom), over
               174 games have been played on my GitHub README with 80,000+
